787cb4d83e1226a166d6a87f31bf28fd04e6863b
Martin Acosta <tinchoz49@gmail.com>
2019-10-25 17:15:10 -0300

    Added missing tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -18,8 +18,8 @@ test('simple', async () => {
   const { alice, bob } = createConnection(
     {
       onmessage: (data) => {
-        expect(data).toBe('ping from bob')
-        return 'pong from alice'
+        expect(data).toEqual(Buffer.from('ping from bob'))
+        return Buffer.from('pong from alice')
       }
     },
     {
@@ -31,7 +31,7 @@ test('simple', async () => {
   )
 
   await expect(alice.request('ping from alice')).resolves.toBe('pong from bob')
-  await expect(bob.request('ping from bob')).resolves.toBe('pong from alice')
+  await expect(bob.request(Buffer.from('ping from bob'))).resolves.toEqual(Buffer.from('pong from alice'))
 })
 
 test('timeout', async () => {
@@ -47,4 +47,34 @@ test('timeout', async () => {
   )
 
   await expect(bob.request('ping from bob')).rejects.toThrow('Timeout on request.')
+})
+
+test('automatic cleanup requests', async () => {
+  const { bob } = createConnection()
+
+  expect(bob._requests.size).toBe(0)
+
+  const ten = Array.from(Array(10).keys()).map(() => bob.request('message'))
+
+  expect(bob._requests.size).toBe(10)
+
+  await Promise.all(ten)
+
+  expect(bob._requests.size).toBe(0)
+})
+
+test('close ar', async () => {
+  const { bob } = createConnection(
+    { onmessage: () => new Promise(resolve => setTimeout(resolve, 1000)) }
+  )
+
+  const finish = expect(bob.request('message')).rejects.toThrow('AbstractRequest close.')
+
+  expect(bob._requests.size).toBe(1)
+
+  setTimeout(() => bob.close(), 1)
+
+  await finish
+
+  expect(bob._requests.size).toBe(0)
 })
