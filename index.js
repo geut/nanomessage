65f438c92d79f8a3f2d4a90de9135d384edb3e2d
Martin Acosta <tinchoz49@gmail.com>
2019-10-30 23:03:29 -0300

    Added jsdoc.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+/**
+ * @typedef {object} NanomessageOptions
+ * @property {sendCallback} [opts.send]
+ * @property {subscribeCallback} [opts.subscribe]
+ * @property {onrequestCallback} [opts.onrequest]
+ * @property {function} [opts.close]
+ * @property {number} [opts.timeout=10000]
+ * @property {Codec} [opts.codec=json]
+ */
+
+/**
+ * Compatible codec: https://github.com/mafintosh/codecs
+ * @typedef {object} Codec
+ * @property {!function} encode
+ * @property {!function} decode
+ */
+
+/**
+ * How to send the data.
+ * @callback sendCallback
+ * @param {Buffer}
+ * @returns {Promise}
+ */
+
+/**
+ * Subscribe for the incoming data.
+ * @callback subscribeCallback
+ * @param {!function} onData
+ * @returns {?function} - Unsubscribe function.
+ */
+
+/**
+ * Request handler.
+ * @callback onrequestCallback
+ * @param {!Buffer} data
+ * @returns {Promise<*>} - Response with any data.
+ */
+
 const Request = require('./lib/request')
 const defaultCodec = require('./lib/codec')
 const {
@@ -17,17 +55,17 @@ const kEncode = Symbol('nanomessage.encode')
 const kDecode = Symbol('nanomessage.decode')
 
 class Nanomessage {
-  static isRequest (request) {
-    return typeof request === 'object' && request._nmId
-  }
-
+  /**
+   * @constructor
+   * @param {NanomessageOptions} [opts]
+   */
   constructor (opts = {}) {
-    const { send, subscribe, close, onrequest, timeout = 10 * 1000, codec = defaultCodec } = opts
+    const { send, subscribe, onrequest, close, timeout = 10 * 1000, codec = defaultCodec } = opts
 
     if (send) this._send = send
     if (subscribe) this._subscribe = subscribe
-    if (close) this._close = close
     if (onrequest) this.setRequestHandler(onrequest)
+    if (close) this._close = close
 
     this[kTimeout] = timeout
     this[kRequests] = new Map()
@@ -36,6 +74,11 @@ class Nanomessage {
     this[kInit]()
   }
 
+  /**
+   * Close the nanomessage, unsubscribe from new incoming data.
+   *
+   * @returns {Promise}
+   */
   async close () {
     this[kSubscription]()
 
@@ -48,6 +91,12 @@ class Nanomessage {
     await this._close()
   }
 
+  /**
+   * Send a new request and wait for a response.
+   *
+   * @param {*} data - Data to be send it.
+   * @returns {Promise<*>} Returns the remote response.
+   */
   async request (data) {
     const request = new Request(data, this[kTimeout])
     this[kRequests].set(request.id, request)
@@ -65,6 +114,11 @@ class Nanomessage {
     }
   }
 
+  /**
+   * Defines the request handler.
+   *
+   * @param {onrequestCallback} cb
+   */
   setRequestHandler (cb) {
     this._onrequest = cb
   }
@@ -106,7 +160,7 @@ class Nanomessage {
       if (!request.nmId) throw new NMSG_ERR_INVALID_REQUEST()
       return request
     } catch (err) {
-      if (err.code === 'NMSG_ERR_INVALID_REQUEST') {
+      if (err instanceof NMSG_ERR_INVALID_REQUEST) {
         throw err
       }
       throw new NMSG_ERR_DECODE(err.message)
@@ -114,6 +168,13 @@ class Nanomessage {
   }
 }
 
+/**
+ * Create a nanomessage from a socket.
+ *
+ * @param {object} socket
+ * @param {NanomessageOptions} [opts]
+ * @returns {Nanomessage}
+ */
 function createFromSocket (socket, options = {}) {
   const nm = new Nanomessage(Object.assign({
     subscribe (ondata) {
@@ -143,7 +204,15 @@ function createFromSocket (socket, options = {}) {
   return nm
 }
 
-module.exports = (...args) => new Nanomessage(...args)
-module.exports.Nanomessage = Nanomessage
-module.exports.createFromSocket = createFromSocket
-module.exports.symbols = { kRequests, kTimeout, kInit, kSubscription, kCodec, kEncode, kDecode }
+/**
+ * Creates a Nanomessage instance.
+ *
+ * @default
+ * @param {NanomessageOptions} [opts]
+ * @returns {Nanomessage}
+ */
+const nanomessage = (opts) => new Nanomessage(opts)
+nanomessage.Nanomessage = Nanomessage
+nanomessage.createFromSocket = createFromSocket
+nanomessage.symbols = { kRequests, kTimeout, kInit, kSubscription, kCodec, kEncode, kDecode }
+module.exports = nanomessage
